5f8cd3e12f4a5bc36d8eb4cf79f73d7c27820aa1
unknown <larserlandson5@gmail.com>
2020-07-14 19:42:05 -0500

    init commit

---
+++ app.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    $.ajax({
+        url: "http://www.devcodecampmusiclibrary.com/api/music",
+        dataType: "json",
+        type: 'get',
+        success: function (data, testStatus, jQxhr) {
+            console.log(data[1]);
+            musicTable = buildTable(data)
+        }
+    });
+})
+
+function buildTable(input) {
+    let table = document.getElementById("musicTable")
+    let row=""
+    for (let i = 0; i < input.length; i++) {
+        row += `<tr>
+            <td>${input[i].title}</td>
+            <td>${input[i].album}</td>
+            <td>${input[i].artist}</td>
+            <td>${input[i].genre}</td>
+            <td>${input[i].releaseDate}</td>
+
+        </tr>`
+        
+    }
+table.innerHTML = row;
+}
+
