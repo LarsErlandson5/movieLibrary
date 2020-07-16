39f880189e12b872dd0284e4d0979543b39f4cfd
gregwilson3468 <gregwilson3468@gmail.com>
2020-07-15 18:52:23 -0600

    added api for all methods with middleware.

--- index.js
+++ index.js
@@ -1,35 +1,36 @@
 const repoContext = require("./repository/repository-wrapper.js");
+const validators = require("./validators/custom-validations.js");
 const express = require('express');
 const app = express();
-const validators = require("./validators/custom-validations.js");
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use((req, res, next) => validators.body(req, res, next));
-app.listen(3000, function () {
-    console.log("Server started. Listening on port 3000.");
+app.listen(3000, function() {
+  console.log("Server started. Listening on port 3000.");
+
 });
-app.get("/api/products", (req, res) => {
-    let products = repoContext.products.findAllProducts();
-    res.send(products);
-})
-app.get("/api/products/:id", (req, res) => {
-    let id = req.params.id;
-    let product = repoContext.procts.findProductById(id);
-    res.send(product);
-})
-app.post("/api/products", (req, res) => {
-    let newProduct = req.body;
-    let addedProduct = repoContext.products.createProduct(newProduct);
-    res.send(addedProduct);
-   });
-app.put("/api/products", (req, res) => {
-    let productToUpdate = req.body;
-    let updatedProduct = repoContext.products.updateProduct(productToUpdate);
-    res.send(updatedProduct);
-   });
-   app.delete("/api/products/:id", (req, res) => {
-    let id = req.params.id;
-    let updatedDataSet =  repoContext.products.deleteProduct(id);
-    res.send(updatedDataSet);
-   });
-   
+app.get("/api/movies", (req, res) => {
+  let movies = repoContext.movies.findAllMovies();
+  res.send(movies);
+});
+app.get("/api/movies/:id", (req, res) => {
+  let id = req.params.id;
+  let movies = repoContext.movies.findMoviesById(id);
+  res.send(movies);
+});
+app.post("/api/movies", (req, res) => {
+let newMovies = req.body;
+let addedMovies = repoContext.movies.createMovies(newMovies);
+res.send(addedMovies);
+});
+app.put("/api/movies", (req, res) => {
+  let moviesToUpdate = req.body;
+  let updatedMovies = repoContext.movies.updateMovies(moviesToUpdate);
+  res.send(updatedMovies);
+});
+app.delete("/api/movies/:id", (req, res) => {
+  let id = req.params.id;
+  let updatedDataSet = repoContext.movies.deleteMovies(id);
+});
