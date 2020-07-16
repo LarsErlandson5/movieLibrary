ee9703bf19c552a4cdb8f43ce02f07ec9be8fd75
gregwilson3468 <gregwilson3468@gmail.com>
2020-07-16 15:33:33 -0600

    Added $PUT and $POST methods and left space for the HTML id's to be inserted. Commented on what needs to be accomplished on the front end.

--- app.js
+++ app.js
@@ -11,20 +11,71 @@ $(document).ready (function () {
     });
 })
 
-function buildTable(movieArray) {
-<<<<<<< HEAD
+function buildTable(movieArray)
     let makeTable = document.getElementById("movieTable")
-=======
+
     let table = document.getElementById("movieTable")
     let row = ""
->>>>>>> 0b86fad5033bdccf732ea39bf3af0dfba529437c
     for (let i = 0; i < movieArray.length; i++) {
             row += `<tr>
             <td>${movieArray[i].title}</td>
             <td>${movieArray[i].director}</td>
             <td>${movieArray[i].genre}</td>
               </tr>`
-           
+
     }
      table.innerHTML = row
 }
+//PUT function
+//Need to add id for event handler
+$("#").submit(editMovies);
+
+function editMovies(event){
+let moviesUpdate = {
+  id: parseInt (this.["id"].value),
+  title: this.["title"].value,
+  director: this.["director"].value,
+  genre: this.["genre"].value,
+}
+$(document).ready(function() {
+$.ajax({
+url: "http://localhost:3000/api/movies",
+dataType: "json",
+type: 'put',
+data: JSON.stringify(moviesUpdate),
+success: function (data) {
+  alert("Update successful!")
+  buildTable(movieArray);
+}
+
+
+
+
+});
+})
+// POST function
+//Need to add id for event handler
+$("#").submit(addMovies);
+function addMovies(event){
+let moviesInfoCreated = {
+  id: parseInt (this.["id"].value),
+  title: this.["title"].value,
+  director: this.["director"].value,
+  genre: this.["genre"].value,
+}
+$(document).ready( function() {
+$.ajax({
+url: "http://localhost:3000/api/movies",
+dataType: "json",
+type: 'post',
+data: JSON.stringify(moviesInfoCreated),
+success: function (data) {
+  alert("Update successful!")
+  buildTable(movieArray);
+}
+
+
+
+
+});
+})
