14062c27d05ef2828287e599510d627bef5f6c11
gregwilson3468 <gregwilson3468@gmail.com>
2020-07-21 18:20:33 -0600

    Corrected errors in post request

--- app.js
+++ app.js
@@ -10,34 +10,17 @@ $(document).ready (function () {
 
 
         }
-    });
-    $("#goput").submit(addMovies);
-     $("go").submit(editMovies);
+    });  $("goput").submit(editMovies);
+         $("go").submit(addMovies);
+
+
 })
 
 
-<<<<<<< HEAD
-function buildTable(movieArray){
-    let makeTable = document.getElementById("movieTable")
-    let table = document.getElementById("movieTable")
-    let row = ""
-    for (let i = 0; i < movieArray.length; i++) {
-            row += `<tr>
-            <td>${movieArray[i].title}</td>
-            <td>${movieArray[i].director}</td>
-            <td>${movieArray[i].genre}</td>
-            <td>${movieArray[i].image}</td>
-              </tr>`
 
-    }
-     table.innerHTML = row
-}
 
-=======
->>>>>>> d68a84dc2ec5bfab33f8515cde4f3948397afd97
-
-$("goput").submit(editMovies);
 function editMovies(e){
+  console.log("hello");
 let moviesUpdate = {
  id: parseInt (this["id"].value),
   title: this["title"].value,
@@ -49,6 +32,7 @@ let moviesUpdate = {
 $.ajax({
 url: "http://localhost:3000/api/movies",
 dataType: "json",
+contentType: "application/json",
 type: 'put',
 data: JSON.stringify(moviesUpdate),
 success: function (data) {
@@ -62,40 +46,18 @@ success: function (data) {
 
 
 
-<<<<<<< HEAD
-});
-})
-
-$("go").submit(addMovies);
-function addMovies(event){
-=======
 
 function addMovies(){
   event.preventDefault();
-
-  console.log("HELLO");
->>>>>>> d68a84dc2ec5bfab33f8515cde4f3948397afd97
+  console.log("hello")
 let moviesInfoCreated = {
   title: this["title"].value,
   director: this["director"].value,
-  genre: this["Genre"].value,
+  genre: this["genre"].value,
 
 }
-console.log('movie', moviesInfoCreated);
+
 $(document).ready( function() {
-<<<<<<< HEAD
-$.ajax({
-url: "http://localhost:3000/api/movies",
-dataType: "json",
-type: 'post',
-data: JSON.stringify(moviesInfoCreated),
-success: function (data) {
-  alert("Update successful!")
-  buildTable(movieArray)
-}
-
-
-=======
   $.ajax({
     url: "http://localhost:3000/api/movies",
     dataType: "json",
@@ -106,7 +68,6 @@ success: function (data) {
       alert("Update successful!")
       buildTable(movieArray);
     }
->>>>>>> d68a84dc2ec5bfab33f8515cde4f3948397afd97
 
     });
   })
@@ -122,7 +83,6 @@ function buildTable(movieArray){
           <td>${movieArray[i].title}</td>
           <td>${movieArray[i].director}</td>
           <td>${movieArray[i].genre}</td>
-          <td>${movieArray[i].image}</td>
             </tr>`
 
   }
